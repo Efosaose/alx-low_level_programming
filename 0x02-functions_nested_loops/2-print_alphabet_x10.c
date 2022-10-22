8c957b5425d5aa559cd8db0faa4b8034bed7dd7a
Efosaose <you@example.com>
2022-10-22 12:39:45 -0700

    task 8

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *  * print_alphabet_x10 - prints the lowercase alphabets
+ *   */
+void print_alphabet_x10(void)
+{
+		int x, j;
+
+			for (x = 0; x <= 9; x++)
+					{
+								for (j = 97; j <= 122; j++)
+											{
+															_putchar(j);
+																	}
+										_putchar('\n');
+											}
+}
